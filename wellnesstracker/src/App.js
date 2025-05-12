3f5c261716160a92f653740f4518a4bde611421c
Joseph Bostok <149004284+Joseph-Bostok@users.noreply.github.com>
2025-05-12 13:22:28 -0400

    added files

---
+++ wellnesstracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HealthQuestDashboard from './healthQuestDash';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HealthQuestDashboard />
+    </div>
+  );
+}
+
+export default App;
