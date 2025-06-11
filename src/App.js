dc66884e581a7bc6c29683fb26879901d0847a35
Joseph Bostok <149004284+Joseph-Bostok@users.noreply.github.com>
2025-06-11 11:28:43 -0400

    f :wq

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import HealthQuestDashboard from './healthQuestDash';
-import LoginForm from './loginForm';
-import RegisterForm from './registerForm';
+import LoginForm from './LoginForm.jsx';
+import RegisterForm from './RegisterForm.jsx';
 import { supabase } from './supabaseClient';
 
 export default function App() {
